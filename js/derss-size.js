fa921b202c131cf138f041ff31ce361860aec9bb
Anurag251 <rana.anurag12345@.com>
2022-03-08 10:02:50 +0545

    update

--- js/derss-size.js
+++ js/derss-size.js
@@ -1,29 +1,38 @@
 const sizes = document.querySelectorAll(".size");
+const checkSizeNull = document.querySelector(".size");
 const sizesInput = document.querySelectorAll("#sizeInput");
+const sizesQuantity = document.querySelectorAll("#sizesQuantity");
 
-sizes.forEach((size, index) => {
-  const sizeLists = size.querySelectorAll("li");
+if (checkSizeNull) {
+  sizes.forEach((size, index) => {
+    const sizeLists = size.querySelectorAll("li");
 
-  const xyz = (e) => {
-    sizesInput[index].setAttribute("value", e);
-  };
+    const productSize = (e) => {
+      sizesInput[index].setAttribute("value", e);
+    };
 
-  sizeLists.forEach((sizeList, idx) => {
-    sizeList.setAttribute("value", sizeList.innerHTML);
+    const sizeQunatity = (e) => {
+      sizesQuantity[index].value = e;
+    };
 
-    sizeList.addEventListener("click", () => {
-      if (sizeList.getAttribute("value") == sizeList.innerHTML) {
-        sizeLists[idx].classList.add("active");
-        choosenSize = idx;
-      }
+    sizeLists.forEach((sizeList, idx) => {
+      sizeList.setAttribute("value", sizeList.innerHTML);
 
-      xyz(sizeList.innerHTML);
-
-      for (let i = 0; i < sizeLists.length; i++) {
-        if (i !== idx) {
-          sizeLists[i].classList.remove("active");
+      sizeList.addEventListener("click", () => {
+        if (sizeList.getAttribute("value") == sizeList.innerHTML) {
+          sizeLists[idx].classList.add("active");
+          choosenSize = idx;
         }
-      }
+
+        productSize(sizeList.innerHTML);
+        sizeQunatity(sizeList.getAttribute("data-value"));
+
+        for (let i = 0; i < sizeLists.length; i++) {
+          if (i !== idx) {
+            sizeLists[i].classList.remove("active");
+          }
+        }
+      });
     });
   });
-});
+}
